64465f5da15ada4f13860e62e22d786e35208941
rodrigo-veiga-10 <rodriveiga10@gmail.com>
2023-11-11 18:45:37 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
+import Home from "./pages/main/home/home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={
+          <Home/>
+        }/>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
